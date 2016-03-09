1275a18085ed53675febf1678ded26de54ce707b
GraceJonn123 <gj.gjonn@gmail.com>
2016-03-09 10:40:29 +0300

    adding landing page

---
+++ app/src/main/java/com/scurrae/chris/instyle/expandable.java
@@ -0,0 +1,14 @@
+package com.scurrae.chris.instyle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class expandable extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exe);
+
+}
+}
